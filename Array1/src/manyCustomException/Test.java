227a0963e48f26dc30fc0d74f795c5344eb71683
tanu <24976766+tanusgit@users.noreply.github.com>
2020-02-24 07:31:38 -0800

    exception

---
+++ Array1/src/manyCustomException/Test.java
@@ -0,0 +1,23 @@
+package manyCustomException;
+
+public class Test {
+	public static void main(String[] args) {
+		
+	}
+
+	
+	
+	public static void validate() {
+		//try {
+		int num1 = 8;
+		int num2 = 2;
+		int res = num1/num2;
+		int array[] = new int[5];
+		System.out.println(res);
+		String name = "";
+		System.out.println(name);
+		//}catch(Exception e) {
+			
+		//}
+	}
+}
