8954f52cb95f35bedff96fad16313bc1052a6324
tanu <24976766+tanusgit@users.noreply.github.com>
2020-03-06 22:54:17 -0800

    word count

--- Array1/src/homeworkonStars/NumberOfText.java
+++ Array1/src/homeworkonStars/NumberOfText.java
@@ -1,16 +1,67 @@
 package homeworkonStars;
 
+import java.util.ArrayList;
+
 public class NumberOfText {
 //Write a method that will return the number of words in a line of text.
 	
 	public static void main(String[] args) {
-		String sentence = "I like to eat pizza a lot";
-		int c = TextCount(sentence);
+		String sentence = "I like tea and you";
+		//System.out.println(sentence.length());
+		int c = WordCount2(sentence);
 		System.out.println(c);
 		
 	}
 
-private static int  TextCount(String sentence) {
+private static int WordCount(String sentence) {
+	String[] n = sentence.split(" ");
+	int res = n.length;
+		return res;
+}
+
+
+
+
+
+private static int WordCount2(String sentence) {
+	ArrayList<String> res = new ArrayList<>();
+	String r  = "";
+	int sp = ' ';
+	for(int i = 0; i < sentence.length(); i ++) {
+		r = r + sentence.charAt(i);
+		if((int)sentence.charAt(i) == sp ) {
+			res.add(r);
+		}
+		
+	}
+	res.add(r);
+	/*for(String n: res) {
+		System.out.println(n);
+	}*/
+	
+	return res.size();
+
+	
+}
+
+
+private static int  WordCount3(String sentence) {
+	int l = sentence.length();
+	int count = 1;
+		for(char r : sentence.toCharArray()) {
+			int res = r;
+			int sp = ' ';
+			if(res == sp) {
+				count++;
+			}
+			
+		}
+		return count;
+	
+}
+
+
+private static int  AlphabetCount(String sentence) {
 	int l = sentence.length();
 	int count = 0;
 		for(char r : sentence.toCharArray()) {
