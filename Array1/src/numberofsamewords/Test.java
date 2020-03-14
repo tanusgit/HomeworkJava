9c2f8c7afc9291824b001de8ac7a67f56551b6aa
tanu <24976766+tanusgit@users.noreply.github.com>
2020-03-13 19:54:47 -0700

    num words

--- Array1/src/numberofsamewords/Test.java
+++ Array1/src/numberofsamewords/Test.java
@@ -10,7 +10,7 @@ public class Test {
 	public static void main(String[] args) {
 		List<String> list = Arrays.asList("java", "php", "android", "sap", "php", "java", "win", "ios", "win", "hana",
 				"android", "ios", "ios", "ios");
-		
+
 		List<String> list2 = new ArrayList<String>();
 		list2.add("java");
 		list2.add("java");
@@ -18,47 +18,41 @@ public class Test {
 		list2.add("php");
 		list2.add("php");
 		list2.add("sap");
-		
 
 		Map<String, Integer> l = findWordCount(list2);
 		System.out.println(l);
-		
-		count(list2);
-		
+
+		// count(list2);
 
 	}
 
 	private static Map<String, Integer> findWordCount(List<String> list) {
 		Map<String, Integer> m = new HashMap<>();
-		int count = 0;
 		for (int i = 0; i < list.size(); i++) {
-			boolean found = false;
-			for (int j = 0; j < m.size(); j++) {
-				if ((m.get(j).equals(list.get(i))) ){
-					found = true;
-				}
-				if(!found) {
-					//Keep p = new Keep(count++, list.get(i));
-					m.put(list.get(i), count++);
-				}
+			boolean exists = m.containsKey(list.get(i));
+			if (exists) {
+				int count = m.get(list.get(i));
+				m.put(list.get(i), count + 1);
+			} else {
+				m.put(list.get(i), 1); //execute for the first time
 			}
 		}
 		return m;
 	}
-	
+
 	public static void count(List<String> list) {
-		
-		int count =0;
+
+		int count = 0;
 		for (int i = 0; i < list.size(); i++) {
 			for (int j = 0; j < list.size(); j++) {
-			if(list.get(j).equals(list.get(i))) {
-				count++;
+				if (list.get(j).equals(list.get(i))) {
+					count++;
+				}
+
 			}
+			System.out.println(list.get(i) + " " + count);
 		}
-		System.out.println(list.get(i)+ " " + count);
+
 	}
-	
-	}
-	
-	
+
 }
