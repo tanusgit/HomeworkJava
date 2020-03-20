87e84969ee39dd599aeb50700137060a9f40cde2
tanu <24976766+tanusgit@users.noreply.github.com>
2020-03-20 10:12:21 -0700

    permutation

--- Array1/src/newhomeworkonCoreJava/Permetutation.java
+++ Array1/src/newhomeworkonCoreJava/Permetutation.java
@@ -2,7 +2,9 @@ package newhomeworkonCoreJava;
 /*Input1 : kumar
 Input2:  2
 Output:
-Ku Km Ka kr um ua ur ma mr ar*/
+Ku Km Ka kr um ua ur ma mr ar
+Kum kua kur uma umr mar
+*/
 import java.util.ArrayList;
 
 public class Permetutation {
@@ -17,28 +19,45 @@ public class Permetutation {
 
 	private static void permuteString(String s, int p) {
 		String res = "";
-		String res2 = "";
 		ArrayList<String> array = new ArrayList<>();
 		for (int i = 0; i < s.length(); i++) {
-			for (int j = 1; j < s.length(); j++) {
+			for (int j = i+1; j < s.length(); j++) {
 				char c = s.charAt(i);
 				char d = s.charAt(j);
-				if (c != d) {
 					res = c + " " + d + "  ";
-					res2 = d + " " + c + "  ";
-					if (!array.contains(res2)) {
+					if (!array.contains(res)) {
 						array.add(res);
 					}
-					// res = "";
-				}
-
 			}
 		}
 		System.out.println(array);
 	}
+	
+	
+	private static void permuteString3(String s, int p) {
+		String res = "";
+		
+		ArrayList<String> array = new ArrayList<>();
+		for (int i = 0; i < s.length(); i++) {
+			for (int j = i+1; j < s.length(); j++) {
+				for (int k = j+1; k < s.length(); k++) {
+				char c = s.charAt(i);
+				char d = s.charAt(j);
+				char e = s.charAt(k);
+					res = c + " " +d + " " + e + "  ";
+					if (!array.contains(res)) {
+						array.add(res);
+					}
+				
+				}
+			}
+		}
+		System.out.println(array);
+	}
+	
 
-	private static void permute(int n, int p) {
-		int res = (factorial(n)) / (factorial(n - p));
+	private static void permute(int n, int r) {
+		int res = factorial(n) / (factorial(n - r)*factorial(r)) ;
 		System.out.println(res);
 
 	}
