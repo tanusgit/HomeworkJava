63dac005f961d8e1cc9799cc23fbee913c4e899c
tanu <24976766+tanusgit@users.noreply.github.com>
2020-05-22 13:50:19 -0700

    math

---
+++ Array1/src/newhomeworkonCoreJava/SomeMath.java
@@ -0,0 +1,17 @@
+package newhomeworkonCoreJava;
+import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.text.NumberFormat;
+import java.util.*;
+/*
+ * we always need to add mysql jar to make the connection with the database
+ * To do that right click on folder name -> build path -> custom build->
+ * add external mysql jar file -> select jar file from downloads or wherever it is
+ * -> then finish
+ */
+public class SomeMath {
+
+}
